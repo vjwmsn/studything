6cc7aa144e8ed9558c507ce92dd7174a4bccdfab
mind <vjwmsn@gmail.com>
2025-08-09 11:49:40 +0200

    first commit!

---
+++ studyapp/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+	
+</style>
+
+{@render children?.()}
---
+++ studyapp/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    let count =  $state(0)
+</script>
+
+<div class="topbar">
+    <button class="regular-button">Leave</button>
+</div>
+
+<style>
+    
+    
+
+    .topbar {
+        display: flex;
+        flex-direction: row;
+        min-height: 3rem;
+        background-color: hsl(0, 0%, 90%);
+        flex: 1;
+        padding: 10px;
+    }
+
+    .regular-button {
+        border: 0;
+        border-radius: 10px;
+        background-color: hsl(0, 100%, 55%);
+        padding: 10px;
+        color: white;
+        opacity: 100%;
+        transition: 0.3s;
+    }
+
+    .regular-button:hover {opacity: 60%;}
+
+</style>
