58332678cec36488632e0e7c18d19dc22e34d3a2
vjwmsn <vjwmsn@gmail.com>
2025-09-03 20:19:33 +0200

    made point resizable prop

---
+++ src/lib/ConvertingPoint.svelte
@@ -0,0 +1,22 @@
+<script>
+  let {active = false, size = "50px"} = $props();
+
+</script>
+
+<div class="point" class:active={active} style="height: {size}; width: {size};"></div>
+
+<style>
+.point {
+  margin: 5px;
+  border-radius: 100%;
+  background: linear-gradient(var(--color-bg-dark-start), var(--color-bg-dark-end));
+  transition: all 0.5s ease;
+}
+
+.point.active {
+  background: linear-gradient(var(--color-accent-1), var(--color-accent-1-90-50));
+  transform: rotate(45deg) scale(0.75);
+  border-radius: 0%;
+  filter: drop-shadow(0 0 0.25rem var(--color-accent-1));
+}
+</style>
---
+++ src/routes/animation-thing/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import TextButton from "$lib/TextButton.svelte";
+  import ConvertingPoint from "$lib/ConvertingPoint.svelte";
+
+  let active = $state(false);
+
+  function changeState() {
+    active = !active;
+  }
+</script>
+
+<div class="point" class:active={active}></div>
+
+<TextButton content="Press me to toggle the animation!" onClick={changeState}/>
+
+<ConvertingPoint active={active}/>
+
+<style>
+.point {
+  height: 50px;
+  width: 50px;
+  margin: 20px;
+  border-radius: 100%;
+  background: linear-gradient(var(--color-bg-dark-start), var(--color-bg-dark-end));
+  transition: all 0.5s ease;
+}
+
+.point.active {
+  background: linear-gradient(var(--color-accent-1), var(--color-accent-1-90-50));
+  transform: rotate(45deg) scale(0.75);
+  border-radius: 0%;
+  filter: drop-shadow(0 0 0.25rem var(--color-accent-1));
+}
+</style>
